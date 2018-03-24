469c4081464330463eea108e3f16a51105c61366
Melinda Hardin <melindawad@gmail.com>
2018-03-24 18:11:09 -0500

    twitter client.get

--- keys.js
+++ keys.js
@@ -2,7 +2,7 @@ console.log('this is loaded');
 
 exports.twitter = {
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
-  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,  // goes to the .env file where the keys are hidden
   access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 };
--- liri.js
+++ liri.js
@@ -1,5 +1,23 @@
-require("dotenv").config(); 
+//require("dotenv").config(); 
+console.log("is this working?");
 
-//console.log("testing my node");
 
-//Add the code required to import the keys.js file and store it in a variable.  <-----LEFT OFF HERE
+//Add the code required to import the keys.js file and store it in a variable.  
+var keys = require("./keys.js");				
+var twitter = require("twitter");				
+var request = require("request");				
+var spotify = require("spotify");				
+
+
+var client = new Twitter(keys.twitter);
+
+var params = {
+    screen_name: 'MelindaYWHardin',
+    count:10
+};
+
+client.get('#######', function(error, tweets, response) {
+    if(error) throw error;
+    console.log(tweets);  // The favorites. (???)
+    console.log(response);  // Raw response object. (???)
+  });
