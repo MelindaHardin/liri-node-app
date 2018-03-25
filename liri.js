383afc173a107e618a2530fb48bc746a13c8f046
Melinda Hardin <melindawad@gmail.com>
2018-03-25 13:13:00 -0500

    notes

--- liri.js
+++ liri.js
@@ -5,19 +5,23 @@ console.log("is this working?");
 //Add the code required to import the keys.js file and store it in a variable.  
 var keys = require("./keys.js");				
 var twitter = require("twitter");				
-var request = require("request");				
-var spotify = require("spotify");				
+var request = require("request"); //<--to grab data from the OMDB API		
+var spotify = require("node-spotify-api");				
 
+var spotify = new Spotify(keys.spotify);
 
 var client = new Twitter(keys.twitter);
 
+//Object that has parameteres (params) in it
 var params = {
     screen_name: 'MelindaYWHardin',
-    count:10
-};
+    count:10  //how many tweets I want back
+}
 
-client.get('#######', function(error, tweets, response) {
+//when the data is returned from the API
+client.get('statuses/user_timeline', function(error, tweets, response) {
+    console.log(data);
     if(error) throw error;
-    console.log(tweets);  // The favorites. (???)
-    console.log(response);  // Raw response object. (???)
+    //console.log(tweets);  // The favorites. (???)
+    //console.log(response);  // Raw response object. (???)
   });
