2e48897898b2b1c63a56b491f8e6eaa8657c8693
Melinda Hardin <melindawad@gmail.com>
2018-03-28 20:23:49 -0500

    twitter connecting..but ugly

--- liri.js
+++ liri.js
@@ -1,23 +1,27 @@
+    //console.log("is this working?");
+var fs = require('fs');
 require("dotenv").config(); 
 
-//console.log("is this working?");
+   //Add the code required to import the keys.js file and store it in a variable.  
+var keys = require("./keys.js");
 
-
-//Add the code required to import the keys.js file and store it in a variable.  
-var keys = require("./keys.js");	
-var twitter = require("twitter");				
-var request = require("request"); //<--to grab data from the OMDB API		
-var spotify = require("node-spotify-api");				
-
-var spotify = new Spotify(keys.spotify);
+    // twitter npm
+var Twitter = require('twitter');
 var client = new Twitter(keys.twitter);
 
+    // spotify npm
+var Spotify = require('node-spotify-api');
+var spotify = new Spotify(keys.spotify);
+
+var request = require("request"); //<--to grab data from the OMDB API		
+				
 var command = process.argv[2];
 var search = process.argv[3];
 
+
 switch(command){
     case 'my-tweets':
-        tweets();
+        getTweets();
     break;
 
     case 'spotify-this-song':
@@ -33,10 +37,7 @@ switch(command){
     break;
 }
 
-//Object that has parameteres (params) in it
-
-
-//twitter function
+//gets 10 Tweets function
 function getTweets(){
 
     var params = {
@@ -45,13 +46,10 @@ function getTweets(){
     }
     
     //when the data is returned from the API
-    client.get('statuses/user_timeline', function(error, tweets, response) {
-        
-        if(error) throw error;
-        console.log(tweets);  // The favorites. (???)
-        console.log(response);  // Raw response object. (???)
-      });
-  
+    client.get('statuses/user_timeline', params, function(error, tweets, response) {
+        if (!error) {
+          console.log(tweets);
+        }
+    });
 
-};
-
+}
